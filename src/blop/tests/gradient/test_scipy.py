e2366bf857058c7ef8a509580b48446e32e03e57
Rhys Takahashi <19mt01@gmail.com>
2026-06-24 14:08:44 -0400

    tiny patch to test now working multithread and imrove thread closure times for testing

--- src/blop/tests/gradient/test_scipy.py
+++ src/blop/tests/gradient/test_scipy.py
@@ -1,3 +1,4 @@
+import time
 from unittest.mock import MagicMock
 
 import pytest
@@ -18,9 +19,11 @@ def mock_evaluation_function():
 def mock_acquisition_plan():
     return MagicMock(spec=AcquisitionPlan)
 
+# agent._optimizer.close() is called so the standard timeout doesnt make the testing take forever
+
 
 @pytest.fixture(scope="function")
-def agent_prep():
+def agent_prep(mock_evaluation_function, mock_acquisition_plan):
     movable1 = MovableSignal(name="test_movable1")
     movable2 = MovableSignal(name="test_movable2")
     readable = ReadableSignal(name="test_readable")
@@ -30,6 +33,7 @@ def agent_prep():
     config = ScipyCFG(
         dofs=[dof1, dof2],
         objective=objective,
+        threads=4
     )
     agent = Scipy(
         sensors=[readable],
@@ -37,7 +41,9 @@ def agent_prep():
         evaluation_function=mock_evaluation_function,
         acquisition_plan=mock_acquisition_plan,
         name="test_experiment",
+        timeout=5
     )
+    time.sleep(.1)
     return agent
 
 
@@ -59,11 +65,13 @@ def test_general_init(mock_evaluation_function, mock_acquisition_plan):
         evaluation_function=mock_evaluation_function,
         acquisition_plan=mock_acquisition_plan,
         name="test_experiment",
+        timeout=5
     )
     assert agent.sensors == [readable]
     assert agent.actuators == [dof1.actuator, dof2.actuator]
     assert agent.evaluation_function == mock_evaluation_function
     assert agent.acquisition_plan == mock_acquisition_plan
+    agent._optimizer.close()
 
 
 def test_agent_init(mock_evaluation_function, mock_acquisition_plan):
@@ -81,11 +89,13 @@ def test_agent_init(mock_evaluation_function, mock_acquisition_plan):
         evaluation_function=mock_evaluation_function,
         acquisition_plan=mock_acquisition_plan,
         name="test_experiment",
+        timeout=5
     )
     assert agent.sensors == [readable]
     assert agent.actuators == [dof1.actuator, dof2.actuator]
     assert agent.evaluation_function == mock_evaluation_function
     assert agent.acquisition_plan == mock_acquisition_plan
+    agent._optimizer.close()
 
 
 def test_agent_to_optimization_problem(mock_evaluation_function):
@@ -100,6 +110,7 @@ def test_agent_to_optimization_problem(mock_evaluation_function):
         dofs=[dof1, dof2],
         objectives=[objective],
         evaluation_function=mock_evaluation_function,
+        timeout=5
     )
     optimization_problem = agent.to_optimization_problem()
     assert optimization_problem.evaluation_function == mock_evaluation_function
@@ -107,6 +118,7 @@ def test_agent_to_optimization_problem(mock_evaluation_function):
     assert optimization_problem.sensors == []
     assert isinstance(optimization_problem.optimizer, ScipyOptimizer)
     assert optimization_problem.acquisition_plan is None
+    agent._optimizer.close()
 
 
 def test_agent_suggest(agent_prep):
@@ -117,33 +129,20 @@ def test_agent_suggest(agent_prep):
     assert "test_movable2" in parameterizations[0]
     assert isinstance(parameterizations[0]["test_movable1"], (int, float))
     assert isinstance(parameterizations[0]["test_movable2"], (int, float))
-
-
-def test_agent_multithread(agent_prep):
-    movable1 = MovableSignal(name="test_movable1")
-    movable2 = MovableSignal(name="test_movable2")
-    readable = ReadableSignal(name="test_readable")
-    dof1 = RangeDOF(actuator=movable1, bounds=(0, 10), parameter_type="float")
-    dof2 = RangeDOF(actuator=movable2, bounds=(0, 10), parameter_type="float")
-    objective = Objective(name="test_objective", minimize=False)
-    config = ScipyCFG(
-        dofs=[dof1, dof2],
-        objective=objective,
-        optimizer=SCP.BFGS,
-        threads=4,
-    )
-    agent = Scipy(
-        sensors=[readable],
-        config=config,
-        evaluation_function=mock_evaluation_function,
-        acquisition_plan=mock_acquisition_plan,
-        name="test_experiment",
-    )
-    parameterizations = agent.suggest(4)
-    print([param.args for param in agent._optimizer._active.values()])
-    assert len(parameterizations) == 4
+    agent_prep._optimizer.close()
 
 
 def test_agent_ingest(agent_prep):
-    suggestions = agent_prep.suggest()
+    agent_prep.suggest()
     agent_prep.ingest([{"test_movable1": 0.1, "test_movable2": 0.2, "test_objective": 0.3, ID_KEY: 0}])
+    agent_prep._optimizer.close()
+
+
+def test_agent_multithread(agent_prep):
+    agent_prep.suggest(1)
+    agent_prep.ingest([{"test_movable1": 0.1, "test_movable2": 0.2, "test_objective": 0.3, ID_KEY: 0}])
+    time.sleep(.1)
+    params = agent_prep.suggest(4)
+    print(agent_prep._optimizer._active)
+    assert len(params) > 1
+    agent_prep._optimizer.close()
