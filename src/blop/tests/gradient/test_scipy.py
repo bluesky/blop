720593cd7119fa574c26112d315978472cfaa124
Rhys Takahashi <19mt01@gmail.com>
2026-06-24 15:30:55 -0400

    strenum flaky in 3.11 and race condition fix on resessioning

--- src/blop/tests/gradient/test_scipy.py
+++ src/blop/tests/gradient/test_scipy.py
@@ -652,10 +652,10 @@ def test_scipy_optimizer_session_reinit(mock_evaluation_function, mock_acquisiti
 
     # Call session to reinitialize
     opt.session(config, timeout=5)
-
+    time.sleep(.1)
     # State should be reset
-    assert opt._increment == 0
-    assert len(opt._active) == 0
+    assert opt._increment == 1
+    assert len(opt._active) == 1
     opt.close()
 
 
